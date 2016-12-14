37974c6a402d8b6b4ff52493203d72b2a864da40
pepperpapa <zhongxin2506@outlook.com>
2016-12-14 16:44:42 +0800

    using NavLink instead Link

--- react-router/modules/Repos.js
+++ react-router/modules/Repos.js
@@ -1,15 +1,16 @@
 import React from "react"
-import { Link } from "react-router"
 
+import NavLink from "./NavLink"
 export default React.createClass({
   render() {
     return (
       <div>
         <h2>Repos</h2>
         <ul>
-          <li><Link to="/repos/reactjs/react-router">React router</Link></li>
-          <li><Link to="/repos/facebook/react">React</Link></li>
+          <li><NavLink to="/repos/reactjs/react-router">React router</NavLink></li>
+          <li><NavLink to="/repos/facebook/react">React</NavLink></li>
         </ul>
+        {this.props.children}
       </div>
     )}
 })
